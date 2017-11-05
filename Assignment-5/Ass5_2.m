e880aa9247a38393e404e4055e6de79445144290
Sachin Antony <sachinantony007@gmail.com>
2017-11-05 14:33:24 -0500

    Committed the file with final changes

--- Assignment-5/Ass5_2.m
+++ Assignment-5/Ass5_2.m
@@ -24,16 +24,6 @@ Xi = merged_data(:,1:rowSize - 1);
 Xi= zscore(Xi);
 yi = merged_data(:,rowSize) ;
 
-
-figure
-hold on
-scatter(Xi(yi==1,1),Xi(yi==1,2),'.b')
-scatter(Xi(yi==-1,1),Xi(yi==-1,2),'.r')
-title('Linearly seperable data')
-xlabel('{x_1}'),ylabel('{x_2}')
-legend('Positive class','Negative class')
-hold off
-
 n = length(Xi(:,1));
 alpha=rand(1,n-1);
 alpha = alpha';
@@ -49,14 +39,8 @@ w=sum(temp);
 for i = 1:n
     KKT1(i) = alpha(i)*(yi(i)*(w*Xi(i,:)'+b(i))-1);
 end
-%KKT1 = eval(KKT);
 [val,i1] = max(KKT1);
 alpha_old = alpha;
-% for i = 1:n
-%     for j = 1: n
-%         tempE(i,j) = alpha(j)*yi(j)*K(i,j);
-%     end
-% end
 for i = 1:n
     E(i) = sum(alpha.*yi.*K(i,:)')-yi(i);
 end
@@ -68,11 +52,11 @@ k = K(i1,i1)+K(i2,i2)-2*K(i1,i2);
 alpha_new = alpha;
 alpha_new(i2) = alpha_old(i2) +  yi(i2)*E(i2)/k;
 if(alpha_new(i2) < 0.001)
-    alpha_new(i2) = 0
+    alpha_new(i2) = 0;
 end
 alpha_new(i1) = alpha_old(i1) + yi(i1)*yi(i2)*(alpha_old(i2)-alpha_new(i2));
 if(alpha_new(i1) < 0.001)
-    alpha_new(i1) = 0
+    alpha_new(i1) = 0;
 end
 
 for i = 1:n
@@ -94,15 +78,7 @@ for i = 1:n
     w = w + alpha(i) * yi(i) * Xi(i, :);
 end
 
-% 
-% b1 = b - E(i1) - yi(i1)*(alpha_new(i1)-alpha_old(i1))*K(i1,i1) - yi(i2)*(alpha_new(i2)-alpha_old(i2))*K(i2,i2);
-% b2 = b - E(i2) - yi(i1)*(alpha_new(i1)-alpha_old(i1))*K(i1,i1) - yi(i2)*(alpha_new(i2)-alpha_old(i2))*K(i2,i2);
-% if b1 == b2
-%     b = b1;
-% else
-%     b = mean([b1 b2]);
-% end
-alpha = alpha_new
+alpha = alpha_new;
 truePositiveCount = 0;
 falsePositiveCount = 0;
 trueNegativeCount = 0;
@@ -127,33 +103,10 @@ end
 
 truePositive = truePositiveCount / (truePositiveCount + falseNegativeCount);
 falsePositive = falsePositiveCount / (falsePositiveCount + falseNegativeCount);
-
-% break if we reached the desired accuracy (based on the param eps)
-% if 1 - (truePositiveCount + trueNegativeCount) / (truePositiveCount + trueNegativeCount + falseNegativeCount + falsePositiveCount) < eps
-%     break
-% end
+accuracy = (truePositiveCount + trueNegativeCount) / (truePositiveCount + trueNegativeCount + falseNegativeCount + falsePositiveCount);
 end
-y=sign(w*Xi'+totalB);
-Xx = w*Xi'+totalB;
+w
+b = totalB
+accuracy
 
-plot(Xx,1);
-confusionmat(yi,y)
 
-figure 
-hold on
-scatter(x(y==1,1),x(y==1,2),'.b')
-scatter(x(y==-1,1),x(y==-1,2),'.r')
-scatter(Xsupport(Ysupport==1,1),Xsupport(Ysupport==1,2),'b')
-scatter(Xsupport(Ysupport==-1,1),Xsupport(Ysupport==-1,2),'r')
-
-syms x1 x2
-fn=vpa((-bias-W*x1)/W(2),5)
-fn1=vpa((-1-bias-W*x1)/W(2),5)
-fn2=vpa((1-bias-W*x1)/W(2),5)
-fplot(fn,'Linewidth',1);
-fplot(fn1,'Linewidth',1);
-fplot(fn2,'Linewidth',1);
-title('Classification using SVM (Linear Kernel & SMO)')
-xlabel('{x_1}'),ylabel('{x_2}')
-legend('Positive class','Negative class')
-hold off
