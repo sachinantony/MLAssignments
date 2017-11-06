43e4a87b56dbda76724d61aaf9c49e6dadaa4e12
Sachin Antony <sachinantony007@gmail.com>
2017-11-05 21:12:09 -0500

    Some changes in the formulae used

--- Assignment-5/Ass5_2.m
+++ Assignment-5/Ass5_2.m
@@ -1,10 +1,9 @@
 clear;
 clear all;
 clc;
-
 load fisheriris;
 
-%replace species names with numerical values
+%Replacing species names
 for i = 1:length(species)
     if strcmp(species(i,1),'setosa')
         species(i,1) = num2cell(1);
@@ -13,7 +12,6 @@ for i = 1:length(species)
     end
 
 end
-C = 0.5;
 numerical_species = cell2mat(species);
 merged_data = horzcat(meas, numerical_species);   
 clear meas;                 %delete meas
@@ -23,22 +21,32 @@ rowSize = length(merged_data(1,:));
 Xi = merged_data(:,1:rowSize - 1);
 Xi= zscore(Xi);
 yi = merged_data(:,rowSize) ;
-
 n = length(Xi(:,1));
+
+%Initialize Alpha
 alpha=rand(1,n-1);
 alpha = alpha';
 prdctSum = sum(yi(1:n-1).*alpha);
 alpha(n)=(-prdctSum)/yi(n);
+
+%Calculate K
 K=Xi*Xi';
+
+
 b = zeros(n,1);
 for zorro = 1:100
+%Calculate w
 for i = 1:n
     temp(i,:) = alpha(i)*yi(i)*Xi(i,:);
 end
 w=sum(temp);
+
+%Calculate KKT
 for i = 1:n
     KKT1(i) = alpha(i)*(yi(i)*(w*Xi(i,:)'+b(i))-1);
 end
+
+%Calculate i1
 [val,i1] = max(KKT1);
 alpha_old = alpha;
 for i = 1:n
@@ -47,16 +55,17 @@ end
 for i = 1:n
     e(i) = E(i1)-E(i);
 end
+
+%Calculate i2
 [val,i2]=max(e);
 k = K(i1,i1)+K(i2,i2)-2*K(i1,i2);
-alpha_new = alpha;
-alpha_new(i2) = alpha_old(i2) +  yi(i2)*E(i2)/k;
-if(alpha_new(i2) < 0.001)
-    alpha_new(i2) = 0;
+alpha(i2) = alpha_old(i2) +  yi(i2)*E(i2)/k;
+if(alpha(i2) < 0.05)
+    alpha(i2) = 0;
 end
-alpha_new(i1) = alpha_old(i1) + yi(i1)*yi(i2)*(alpha_old(i2)-alpha_new(i2));
-if(alpha_new(i1) < 0.001)
-    alpha_new(i1) = 0;
+alpha(i1) = alpha_old(i1) + yi(i1)*yi(i2)*(alpha_old(i2)-alpha(i2));
+if(alpha(i1) < 0.05)
+    alpha(i1) = 0;
 end
 
 for i = 1:n
@@ -65,27 +74,20 @@ for i = 1:n
     end
 end
 
-bCount = 0;
-for i = 1:n
-    if alpha(i) ~= 0
-        bCount = bCount + 1;
-    end
-end
-totalB = sum(b) / bCount;
+bias = mean(b);
 
 w = zeros(1, length(Xi(1,:)));
 for i = 1:n
     w = w + alpha(i) * yi(i) * Xi(i, :);
 end
 
-alpha = alpha_new;
 truePositiveCount = 0;
 falsePositiveCount = 0;
 trueNegativeCount = 0;
 falseNegativeCount = 0;
 
 for i = 1:n
-    output = sign(w*Xi(i, :)' + totalB);
+    output = sign(w*Xi(i, :)' + bias);
     if output == 1
        if output == yi(i)
            truePositiveCount = truePositiveCount + 1;
@@ -104,9 +106,12 @@ end
 truePositive = truePositiveCount / (truePositiveCount + falseNegativeCount);
 falsePositive = falsePositiveCount / (falsePositiveCount + falseNegativeCount);
 accuracy = (truePositiveCount + trueNegativeCount) / (truePositiveCount + trueNegativeCount + falseNegativeCount + falsePositiveCount);
+if accuracy > .98
+    break
+end
 end
 w
-b = totalB
+b = bias
 accuracy
 
 
